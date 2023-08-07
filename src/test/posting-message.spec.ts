acdf2bf52fde64eb0c438b97c46c8fc5ce73ef7c
Tonyp85-25 <anthony.paulin@enil-mamirolle.fr>
2023-08-07 14:17:26 +0200

    test structure done

---
+++ src/test/posting-message.spec.ts
@@ -0,0 +1,46 @@
+describe('Feature: posting a message',()=>{
+    describe('Rule: a message can contain a maximum of 280 characters',()=>{
+        test("Alice can post a message on her timeline", ()=>{
+            givenNowIs(new Date("2023-01-19T19:00:00.000Z"))
+            whenUserPostsAMessage({
+                id:"message-id",
+                text:"Hello World",
+                author:"Alice"
+            })
+            thenPostedMessageShouldBe({
+                id:"message-id",
+                text:"Hello World",
+                author:"Alice",
+                publishedAt:new Date("2023-01-19T19:00:00.000Z")
+            })
+        })
+    }
+    )
+})
+
+let message : {
+    id:string;
+    text:string
+    author:string,
+    publishedAt:Date
+}
+
+let now:Date
+
+function givenNowIs(_now:Date)
+{
+    now =  _now
+}
+
+function whenUserPostsAMessage(postMessageCommand:{id:string, text:string, author:string})
+{
+    message = {id:postMessageCommand.id,
+         text:postMessageCommand.text,
+          author:postMessageCommand.author,
+           publishedAt:now}
+}
+
+function thenPostedMessageShouldBe(expectedMessage:{id:string, text:string, author:string, publishedAt:Date})
+{
+    expect(expectedMessage).toEqual(message)
+}
