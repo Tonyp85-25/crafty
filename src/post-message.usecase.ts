cfbf376b71b5adb89c86ca430a0fe5ff5ec27c1e
Tonyp85-25 <anthony.paulin@enil-mamirolle.fr>
2023-08-08 00:03:46 +0200

    post creation and post length rule added

---
+++ src/post-message.usecase.ts
@@ -0,0 +1,40 @@
+export type PostMessageCommand = {
+  id: string;
+  text: string;
+  author: string;
+};
+
+export type Message = {
+  id: string;
+  text: string;
+  author: string;
+  publishedAt: Date;
+};
+
+export interface MessageRepository {
+  save(message: Message): void;
+}
+
+export interface DateProvider {
+  getNow(): Date;
+}
+
+export class MessageTooLongError extends Error {}
+
+export class PostMessageUseCase {
+  constructor(
+    private readonly messageRepository: MessageRepository,
+    private readonly dateProvider: DateProvider
+  ) {}
+  handle(postMessageCommand: PostMessageCommand) {
+    if (postMessageCommand.text.length > 280) {
+      throw new MessageTooLongError();
+    }
+    this.messageRepository.save({
+      id: postMessageCommand.id,
+      text: postMessageCommand.text,
+      author: postMessageCommand.author,
+      publishedAt: this.dateProvider.getNow(),
+    });
+  }
+}
--- src/test/posting-message.spec.ts
+++ src/test/posting-message.spec.ts
@@ -1,46 +1,91 @@
-describe('Feature: posting a message',()=>{
-    describe('Rule: a message can contain a maximum of 280 characters',()=>{
-        test("Alice can post a message on her timeline", ()=>{
-            givenNowIs(new Date("2023-01-19T19:00:00.000Z"))
-            whenUserPostsAMessage({
-                id:"message-id",
-                text:"Hello World",
-                author:"Alice"
-            })
-            thenPostedMessageShouldBe({
-                id:"message-id",
-                text:"Hello World",
-                author:"Alice",
-                publishedAt:new Date("2023-01-19T19:00:00.000Z")
-            })
-        })
-    }
-    )
-})
+import {
+  DateProvider,
+  Message,
+  MessageRepository,
+  MessageTooLongError,
+  PostMessageUseCase,
+} from "../post-message.usecase";
 
-let message : {
-    id:string;
-    text:string
-    author:string,
-    publishedAt:Date
+describe("Feature: posting a message", () => {
+  describe("Rule: a message can contain a maximum of 280 characters", () => {
+    test("Alice can post a message on her timeline", () => {
+      givenNowIs(new Date("2023-01-19T19:00:00.000Z"));
+      whenUserPostsAMessage({
+        id: "message-id",
+        text: "Hello World",
+        author: "Alice",
+      });
+      thenPostedMessageShouldBe({
+        id: "message-id",
+        text: "Hello World",
+        author: "Alice",
+        publishedAt: new Date("2023-01-19T19:00:00.000Z"),
+      });
+    });
+    test("Too long message generates error", () => {
+      let textOf281 =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a ante quis enim iaculis scelerisque. Nulla interdum mauris id justo hendrerit, vitae vulputate justo sagittis. Integer posuere lacus finibus, aliquet diam sed, placerat sem. Curabitur gravida luctus tortor, ac nunc.";
+      givenNowIs(new Date("2023-01-19T19:00:00.000Z"));
+      whenUserPostsAMessage({
+        id: "message-id",
+        text: textOf281,
+        author: "Alice",
+      });
+
+      thenErrorShouldBe(MessageTooLongError);
+    });
+  });
+});
+
+let message: Message;
+let thrownError: Error;
+
+class InMemoryMessageRepository implements MessageRepository {
+  save(msg: Message): void {
+    message = msg;
+  }
 }
 
-let now:Date
-
-function givenNowIs(_now:Date)
-{
-    now =  _now
+class StubDateProvider implements DateProvider {
+  now: Date;
+  getNow(): Date {
+    return this.now;
+  }
 }
 
-function whenUserPostsAMessage(postMessageCommand:{id:string, text:string, author:string})
-{
-    message = {id:postMessageCommand.id,
-         text:postMessageCommand.text,
-          author:postMessageCommand.author,
-           publishedAt:now}
+const messageRepository = new InMemoryMessageRepository();
+const dateProvider = new StubDateProvider();
+
+const postMessageUseCase = new PostMessageUseCase(
+  messageRepository,
+  dateProvider
+);
+
+function givenNowIs(_now: Date) {
+  dateProvider.now = _now;
 }
 
-function thenPostedMessageShouldBe(expectedMessage:{id:string, text:string, author:string, publishedAt:Date})
-{
-    expect(expectedMessage).toEqual(message)
+function whenUserPostsAMessage(postMessageCommand: {
+  id: string;
+  text: string;
+  author: string;
+}) {
+  try {
+    postMessageUseCase.handle(postMessageCommand);
+  } catch (err) {
+    thrownError = err;
+  }
+}
+
+function thenPostedMessageShouldBe(expectedMessage: {
+  id: string;
+  text: string;
+  author: string;
+  publishedAt: Date;
+}) {
+  expect(expectedMessage).toEqual(message);
+}
+
+function thenErrorShouldBe(expectedErrorClass: new () => Error) {
+  expect(thrownError).toBeInstanceOf(expectedErrorClass);
 }
